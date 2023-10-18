c3020c46cb0debdf4570eb031bca67aace8e41a7
Naresh-SV <salevaddi.naresh2468@gmail.com>
2023-10-18 17:50:19 +0530

    Get and Post Request APIs test script

---
+++ RestAssuredTraining/src/test/java/restAssuredTestScripts/PostRequestWithHashMapTestData.java
@@ -0,0 +1,45 @@
+package restAssuredTestScripts;
+
+import org.testng.annotations.Test;
+
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+import java.util.HashMap;
+
+public class PostRequestWithHashMapTestData {
+	int ID;
+	@Test(priority=1)
+	void postRequestValidation() {
+		HashMap data=new HashMap();
+		data.put("name","Naresh");
+		data.put("job", "Software");
+		
+		Response rs=given()
+			.contentType("application/json")
+			.body(data)
+		.when()
+			.post("https://reqres.in/api/users");
+		
+		ID=rs.getBody().jsonPath().getInt("id");
+	}
+	
+	@Test(priority=2, dependsOnMethods={"postRequestValidation"})
+	void updatePreviousRequest() {
+		HashMap data=new HashMap();
+		data.put("name","Naresh");
+		data.put("job", "Software Engineer");
+		given()
+			.contentType("application/json")
+			.body(data)
+		.when()
+			.put("https://reqres.in/api/users/"+ID)
+		.then()
+			.statusCode(200)
+			.log().all();
+	}
+
+}
